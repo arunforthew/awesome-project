c9bfc24816c34641e8a038d61b8b765436b5d01c
arunforthew <arunforthew@gmail.com>
2024-07-11 14:03:24 +0530

    Hi

---
+++ src/test/java/com/git/A.java
@@ -0,0 +1,22 @@
+package com.git;
+
+public class A {
+	private void m1() {
+		System.out.println("1");
+
+
+	}
+	private void m2() {
+		System.out.println("2");
+
+
+	}
+	private void m3() {
+		System.out.println("3");
+
+
+	}
+
+
+
+}
